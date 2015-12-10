71adf913966508fe54008bb302263c19a7ba2bd2
Nikolaj Majorov <nmajorov@redhat.com>
2015-12-10 13:04:55 +0100

    fix Dynamic router

--- src/main/java/biz/majorov/camel/HeadersDynamicRouter.java
+++ src/main/java/biz/majorov/camel/HeadersDynamicRouter.java
@@ -4,15 +4,26 @@ import org.apache.camel.Header;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+/**
+ * Dynamic router class 
+ * 
+ * @author Nikolaj Majorov
+ *
+ */
 public class HeadersDynamicRouter {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(HeadersDynamicRouter.class);
 	
-	public String routing(String body, @Header("X-MySpecialHeader") String specialHeader) {
+	
+	public String routing(@Header("X-MySpecialHeader") String specialHeader,
+			@Header("parsed") Boolean parsed) {
 		
 		LOG.info("routing called  specialHeader: " + specialHeader);
 		
+		if (parsed != null && parsed == true){
+		//	LOG .info("exchange: " + endpoint.createExchange());
+			return null;
+		}
         if (specialHeader == null) {
             return "direct:direct";
         } else  {
